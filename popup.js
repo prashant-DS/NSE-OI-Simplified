9dc00e4c692a603dfdfb7eddb33365b2c487bc19
Prashant <pk8873444412@gmail.com>
2023-12-11 15:39:43 +0530

    fetched all

--- popup.js
+++ popup.js
@@ -1,56 +1,42 @@
 // popup.js
 document.addEventListener("DOMContentLoaded", function () {
-  // Check if the data is present in localStorage
-  const myData = localStorage.getItem("yourDataKey");
-
-  // Check if the data is present and display accordingly
-  if (myData) {
-    contentDiv.textContent = `Data found: ${myData}`;
-  } else {
-    // Data not found, make a request to your API endpoint
-    fetch("https://www.nseindia.com/api/option-chain-indices?symbol=BANKNIFTY")
-      .then((response) => response.json())
-      .then((data) => {
-        const {
-          strikePriceMaxCE,
-          strikePriceMaxPE,
-          maxCEOpenInterest,
-          maxPEOpenInterest,
-        } = getMaxOE(data);
-
-        console.log(
-          "Strike Price for Max Open Interest in CE:",
-          strikePriceMaxCE,
-          maxCEOpenInterest
-        );
-        console.log(
-          "Strike Price for Max Open Interest in PE:",
-          strikePriceMaxPE,
-          maxPEOpenInterest
-        );
-
-        // final
-        document.getElementById("bankResOI").textContent = maxCEOpenInterest;
-        document.getElementById("bankResPrice").textContent = strikePriceMaxCE;
-
-        document.getElementById("bankCurrentPrice").textContent =
-          data.records.index.last;
-
-        document.getElementById("bankSuppOI").textContent = maxPEOpenInterest;
-        document.getElementById("bankSuppPrice").textContent = strikePriceMaxPE;
-      })
-      .catch((error) => {
-        console.error("Error fetching data from the API:", error);
-        contentDiv.textContent = "Error fetching data from the API";
-      });
-  }
-
-  document
-    .getElementById("refreshAllButton")
-    .addEventListener("click", function () {
-      chrome.runtime.reload();
-    });
+  fetchForIndex("BANKNIFTY", "bank");
+  fetchForIndex("NIFTY", "nifty");
+  fetchForIndex("MIDCPNIFTY", "midcap");
 });
+
+function fetchForIndex(index, id) {
+  console.log(
+    `https://www.nseindia.com/api/option-chain-indices?symbol=${index}`
+  );
+  fetch(`https://www.nseindia.com/api/option-chain-indices?symbol=${index}`)
+    .then((response) => response.json())
+    .then((data) => {
+      // console.log("🥳 ~ file: popup.js:14 ~ .then ~ data:", data);
+      const {
+        strikePriceMaxCE,
+        strikePriceMaxPE,
+        maxCEOpenInterest,
+        maxPEOpenInterest,
+      } = getMaxOE(data);
+
+      document.getElementById(id + "ResOI").textContent = maxCEOpenInterest;
+      document.getElementById(id + "ResPrice").textContent = strikePriceMaxCE;
+
+      document.getElementById(id + "CurrentPrice").textContent =
+        Math.round(data.records?.index?.last) ||
+        Math.round(data.records?.underlyingValue) ||
+        "-";
+
+      document.getElementById(id + "SuppOI").textContent = maxPEOpenInterest;
+      document.getElementById(id + "SuppPrice").textContent = strikePriceMaxPE;
+    })
+    .catch((error) => {
+      console.error("Error fetching data from the API:", error);
+      document.body.textContent =
+        "Error fetching data from the API   : :   " + error.message;
+    });
+}
 
 function getMaxOE(data) {
   let maxCEOpenInterest = 0;
